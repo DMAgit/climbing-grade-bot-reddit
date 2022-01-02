0afc302b96b3c9425d80889fd6e53931bb709461
DMAgit <dmapostbox@gmail.com>
2022-01-02 16:29:46 +0100

    Processing words in comments now. made RE for V-grade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import praw
 import config
+import re
 
 reddit = praw.Reddit(
     client_id=config.client_id,
@@ -13,14 +14,38 @@ reddit = praw.Reddit(
 target_sub = "climbing+bouldering+climbharder"
 subreddit = reddit.subreddit(target_sub)
 
+# RegEx
+re_V = "^[V,v]([1][0-9]|[0-9])\b"
+re_YDS = ""
 
-def posts():
+# for submission in subreddit.stream.submissions():
+#     print(submission.title)
+
+for comment in subreddit.stream.comments():
+    # we don't want to convert comments we or MountainProjectBot made
+    if not comment.author == "MountainProjectBot" or "climb-grade-bot":
+        for word in comment.body.split():
+            if re.search(re_V, word):  # check if the word is a V-grade
+                pass
+            elif re.search(re_YDS, word):  # check if the word is a YDS grade
+                pass
+
+
+def v_to_french(grade):
+    """
+    Convert from V-grade to French boulder grade
+    :param grade: String
+    :return converted_grade:
+    """
     pass
-    for submission in subreddit.stream.submissions():
-        print(submission.title)
+    return converted_grade
 
 
-def comments():
+def YDS_to_french(grade):
+    """
+    Convert from YDS grade to French sport climbing grade
+    :param grade: String
+    :return converted_grade:
+    """
     pass
-    for comment in subreddit.stream.comments():
-        print(comment.body)
+    return converted_grade
